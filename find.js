01e46c2e7f4cb3a805bed7fe12eac76fcfe47a2c
Nithin KS <50230678+nithinkodadhavadee@users.noreply.github.com>
2021-10-21 20:25:18 +0530

    added a projectin to the find method

--- find.js
+++ find.js
@@ -4,7 +4,8 @@ var url = "mongodb+srv://admin:admin@cluster0.sfvva.mongodb.net/";
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo = db.db("anotherSecondDatabase");
-  dbo.collection("anythingWillWork").find({}).toArray(function(err, result) {
+//   dbo.collection("anythingWillWork").find({}).toArray(function(err, result) {
+  dbo.collection("anythingWillWork").find({}, {projection: {_id:0, name:1, email:1}}).toArray(function(err, result) {
     if (err) throw err;
     console.log(result);
     db.close();
